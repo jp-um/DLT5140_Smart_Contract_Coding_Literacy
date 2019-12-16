ff27d4de5735330974c463eb032290750dd1661c
JP <jp@javaclass.co.uk>
2019-12-16 10:26:54 +0100

    Betting game

---
+++ Bet.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.5.12;
+
+contract Gambling {
+    
+    enum STATUS { AWAIT_BET, AWAIT_GUESS, AWAIT_RESULT }
+    enum COIN { HEAD, TAIL }
+    
+	STATUS betStatus;	
+	COIN guess;
+	uint betAmount;
+    address payable public jane;
+    address payable public peter;
+    
+	constructor(address payable _jane, address payable _peter) public {
+    	betStatus = STATUS.AWAIT_BET;
+    	jane = _jane;
+    	peter = _peter;
+	}
+
+    function createBet() public payable {
+    	require (betStatus == STATUS.AWAIT_BET);
+        require (msg.sender == jane);
+
+    	betAmount = msg.value;	
+    	betStatus = STATUS.AWAIT_GUESS;
+	}
+
+	function makeGuess(COIN g) public payable {
+    	require (betStatus == STATUS.AWAIT_GUESS);
+        require (msg.sender == peter);
+        require (msg.value == betAmount);
+	
+    	guess = g;
+	    betStatus = STATUS.AWAIT_RESULT;
+	}
+
+	function revealCoin(COIN toss) public {
+	    require (betStatus == STATUS.AWAIT_RESULT);
+        require (msg.sender == jane);
+	
+    	if (guess == toss) {
+            peter.transfer(betAmount * 2);
+        } else {
+            jane.transfer(betAmount * 2);
+        }
+ 
+    	betStatus = STATUS.AWAIT_BET;
+    }
+	
+}
